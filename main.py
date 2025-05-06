3c0f35a3ce7a1828c9832de6dca3aa5626014bfb
Gray-Tech <joegray1@live.co.uk>
2025-05-06 07:53:19 +0100

    Counts all words

--- main.py
+++ main.py
@@ -3,14 +3,11 @@ def get_book_text(filepath):
         text = f.read()
     return text
 def count_words(text):
-    count = 0
-    text_list = text.split(" ")
-    for word in text_list():
-        count = count + 1
-    return count
+    return len(text.split())
+
 
 def main():
-    text = get_book_text("./books/frankenstein.txt")
+    text = get_book_text("/home/user/workspace/github.com/gray-tech/bookbot/books/frankenstein.txt")
     word_count = count_words(text)
     print(f"{word_count} words found in the document")
 
